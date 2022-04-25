b5ae9c21e258fc40439d268091af6c7ec7cb8380
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-24 20:39:10 -0700

    created app/updated css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kelsey Murray and is open-sourced on{" "}
-        <a
-          href="https://github.com/artistkelz/my-weather-project-react2022"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <br />
+        <Weather />
+        <footer>
+          This project was coded by Kelsey Murray and is open-sourced on{" "}
+          <a
+            href="https://github.com/artistkelz/my-weather-project-react2022"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
