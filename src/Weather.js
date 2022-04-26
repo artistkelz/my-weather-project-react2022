ee794b44b1d7e829aca48310eb3ff122ef74445b
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-25 23:40:25 -0700

    Updated Styling

--- src/Weather.js
+++ src/Weather.js
@@ -55,7 +55,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="search"
-                className="btn btn-primary w-100"
+                className="btn btn-info w-100"
               />
             </div>
           </div>
